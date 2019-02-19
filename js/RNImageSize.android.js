ac77c1d227538ebd481e691181add16fe015a3a3
Ivan Sorokin <ivan.-94@mail.ru>
2019-02-20 00:28:06 +0300

    Update RNImageSize.android.js

--- js/RNImageSize.android.js
+++ js/RNImageSize.android.js
@@ -3,6 +3,4 @@ import { NativeModules } from 'react-native';
 
 const { RNImageSize } = NativeModules;
 
-console.info('!!!', { NativeModules, RNImageSize });
-
 export default { getSize: RNImageSize.getSize };
